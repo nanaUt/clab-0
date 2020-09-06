37f62fede662b1c285d56aeef06384b26168b4a0
nanaUt <70767069+nanaUt@users.noreply.github.com>
2020-09-06 15:51:28 +0300

    Add files via upload

--- equation.c
+++ equation.c
@@ -1,29 +1,21 @@
-//a*x^2+b*x+c
-
-#include <stdio.h> 
+//a*x^2+b*x+c=0
+#include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-
 int main()
 {
-
 int a, b, c, d;
 float x1, x2, x3;
-
 printf ("Введите a = ");
 scanf ("%d", &a);
-
 printf ("Введите b = ");
 scanf ("%d", &b);
-
 printf ("Введите c = ");
 scanf ("%d", &c);
-
 d = (b * b) - 4 * a * c;
 x1 = (-b + sqrt(d)) / (2 * a);
 x2 = (-b - sqrt(d)) / (2 * a);
 x3 = (-b) / (2 * a);
-
 if (d > 0) {
 printf ("x1 = %f\n", x1);
 printf ("x2 = %f\n", x2);
